68a76774dcfeb0dedfe973c41ddeeb42592f1845
HarukiHaruki <coral.reef610@gmail.com>
2017-09-17 14:25:59 +0900

    JS／CSS類追加

--- resources/views/layouts/footer/build_footer.blade.php
+++ resources/views/layouts/footer/build_footer.blade.php
@@ -1,4 +1,11 @@
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jquery-3.2.1.js') }}"></script>
+<script type="text/javascript" src="{{ asset('css/Semantic-UI-CSS/semantic.min.js') }}"></script>
+<script type="text/javascript">
+    $('.ui.dropdown').dropdown();
+</script>
+<script type='text/javascript' src="{{ asset('js/vis.js/dist/vis.min.js') }}"></script>
+<script type='text/javascript' src="{{ asset('js/CreateModel_sample.js') }}"></script>
 </body>
 </html>
