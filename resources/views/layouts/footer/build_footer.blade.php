3bc0194d2e12a4fc7f131240f02ae8e6f47834de
HarukiHaruki <coral.reef610@gmail.com>
2017-10-02 10:58:59 +0900

    vue.jsを追加

--- resources/views/layouts/footer/build_footer.blade.php
+++ resources/views/layouts/footer/build_footer.blade.php
@@ -1,5 +1,6 @@
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
+<script src="https://unpkg.com/vue"></script>
 <script type="text/javascript" src="{{ asset('js/jquery-3.2.1.js') }}"></script>
 <script type="text/javascript" src="{{ asset('css/Semantic-UI-CSS/semantic.min.js') }}"></script>
 <script type="text/javascript">
