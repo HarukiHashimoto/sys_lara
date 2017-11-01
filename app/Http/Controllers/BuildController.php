ea85cf02db470c54c883d2c5498db406508367e7
Haruki-MacMini <coral.reef610@gmail.com>
2017-11-01 17:25:09 +0900

    Ajax通信をaxiosで実現

--- app/Http/Controllers/BuildController.php
+++ app/Http/Controllers/BuildController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
@@ -16,7 +16,14 @@ class BuildController extends Controller
 
     public function build_smp()
     {
-        
         return view('build_smp');
+    }
+
+    public function save_model()
+    {
+        $smp = Request::input('sample');
+        echo $smp;
+        log::info(auth::id());
+        // return auth::user();
     }
 }
