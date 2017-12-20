ebb1c4ca7fa0c16e61e2d6463abae2c73b9fcf4f
Haruki-MacMini <coral.reef610@gmail.com>
2017-12-20 17:22:29 +0900

    CSS更新

--- resources/views/build_smp.blade.php
+++ resources/views/build_smp.blade.php
@@ -5,100 +5,101 @@
 @endsection
 
 @section('content')
-<div>
-    @if (session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
+    <div id="content">
+        <div id="wrapper">
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+            <div class="ui grid">
+                <div class="row">
+                    <div class="nine wide column">
+                        <div id="node-popUp">
+                          <span id="node-operation">node</span> <br>
+                          <table style="margin:auto;">
+                            <tr>
+                              <td>id</td><td><input id="node-id" value="new value" /></td>
+                            </tr>
+                            <tr>
+                              <td>内容</td><td><textarea id="node-label" value="new value" ></textarea></td>
+                            </tr>
+                            <tr>
+                              <td>group</td><td><input id="group" value="usr_node" /></td>
+                            </tr>
+                          </table>
+                          <input type="button" value="save" id="node-saveButton" />
+                          <input type="button" value="cancel" id="node-cancelButton" />
+                        </div>
+
+                        <div id="edge-popUp">
+                            <span id="edge-operation">edge</span> <br>
+                            <table style="margin:auto;">
+                                <tr>
+                                    {{-- <td>P or N</td> --}}
+                                    {{-- <td><input id="edge-label" value="new value" /></td> --}}
+                                    <td><p>
+                                    <input type="radio" name="pon" value="1" /> Posi
+                                    <input type="radio" name="pon" value="2" /> Nega
+                                    <input type="radio" name="pon" value="3" /> else
+                                    </p></td>
+                                </tr>
+                            </table>
+                          <input type="button" value="save" id="edge-saveButton" />
+                          <input type="button" value="cancel" id="edge-cancelButton" />
+                        </div>
+                        <div id="mymodel_s" class="modelArea">
+
+                        </div>
+                    </div>
+                    <div class="four wide column">
+                        <br>
+                        <br>
+                        <h5>タグの付与</h5>
+                        <p>※選択中のノードにタグを付与します．</p>
+                        <div class="ui inverted segment">
+                            <button class="ui button inverted tag yellow" value="0">提案</button>
+                            <button class="ui button inverted tag purple" value="1">指針</button>
+                            <button class="ui button inverted tag grey" value="2">結論</button>
+                            <button class="ui button inverted tag blue" value="3">問題</button>
+                            <button class="ui button inverted tag teal" value="4">関与者</button>
+                            <button class="ui button inverted tag red" value="5">懸念</button>
+                            <button class="ui button inverted tag green" value="6">問い</button>
+                            <button class="ui button inverted tag brown" value="7">答え</button>
+                        </div>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        {{-- 問いリスト --}}
+                        <qlist></qlist>
+                    </div>
+
+                    <div class="three wide column">
+
+                    </div>
+                </div>
+            </div>
+            <div class="ui centered grid">
+                <button class="save_btn ui huge button primary">保存</button>
+                <button class="ref_btn ui huge button positive">参照</button>
+            </div>
+            <br>
+            <br>
+            <br>
+
         </div>
-    @endif
-    <div class="ui grid">
-        <div class="row">
-            <div class="nine wide column">
-                <div id="node-popUp">
-                  <span id="node-operation">node</span> <br>
-                  <table style="margin:auto;">
-                    <tr>
-                      <td>id</td><td><input id="node-id" value="new value" /></td>
-                    </tr>
-                    <tr>
-                      <td>内容</td><td><textarea id="node-label" value="new value" ></textarea></td>
-                    </tr>
-                    <tr>
-                      <td>group</td><td><input id="group" value="usr_node" /></td>
-                    </tr>
-                  </table>
-                  <input type="button" value="save" id="node-saveButton" />
-                  <input type="button" value="cancel" id="node-cancelButton" />
-                </div>
 
-                <div id="edge-popUp">
-                    <span id="edge-operation">edge</span> <br>
-                    <table style="margin:auto;">
-                        <tr>
-                            {{-- <td>P or N</td> --}}
-                            {{-- <td><input id="edge-label" value="new value" /></td> --}}
-                            <td><p>
-                            <input type="radio" name="pon" value="1" /> Posi
-                            <input type="radio" name="pon" value="2" /> Nega
-                            <input type="radio" name="pon" value="3" /> else
-                            </p></td>
-                        </tr>
-                    </table>
-                  <input type="button" value="save" id="edge-saveButton" />
-                  <input type="button" value="cancel" id="edge-cancelButton" />
-                </div>
-                <div id="mymodel_s" class="modelArea">
-
-                </div>
+        <div class="ui modal basic ref_modal">
+            <div class="header">
+                Browse Other Model
             </div>
-            <div class="four wide column">
-                <br>
-                <br>
-                <h5>タグの付与</h5>
-                <p>※選択中のノードにタグを付与します．</p>
-                <div class="ui inverted segment">
-                    <button class="ui button inverted tag yellow" value="0">提案</button>
-                    <button class="ui button inverted tag purple" value="1">指針</button>
-                    <button class="ui button inverted tag grey" value="2">結論</button>
-                    <button class="ui button inverted tag blue" value="3">問題</button>
-                    <button class="ui button inverted tag teal" value="4">関与者</button>
-                    <button class="ui button inverted tag red" value="5">懸念</button>
-                    <button class="ui button inverted tag green" value="6">問い</button>
-                    <button class="ui button inverted tag brown" value="7">答え</button>
-                </div>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                {{-- 問いリスト --}}
-                <qlist></qlist>
-            </div>
-
-            <div class="three wide column">
+            <div class="refModelArea" id="refModel">
 
             </div>
         </div>
     </div>
-    <div class="ui centered grid">
-        <button class="save_btn ui huge button primary">保存</button>
-        <button class="ref_btn ui huge button positive">参照</button>
-    </div>
-    <br>
-    <br>
-    <br>
-
-</div>
-
-<div class="ui modal basic ref_modal">
-    <div class="header">
-        Browse Other Model
-    </div>
-    <div class="refModelArea" id="refModel">
-
-    </div>
-</div>
-
 @endsection
 
 
