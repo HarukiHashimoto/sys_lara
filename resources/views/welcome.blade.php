49f6ee6c2cf3b8b6ef4f5ca742c56a99effd7659
Haruki-MacMini <coral.reef610@gmail.com>
2018-03-13 16:33:10 +0900

    refactor: topページからのリンクをindex.phpありに変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,8 +71,8 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="index.php/login">Login</a>
+                        <a href="index.php/register">Register</a>
                     @endauth
                 </div>
             @endif
@@ -90,8 +90,8 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="index.php/login">Login</a>
+                        <a href="index.php/register">Register</a>
                     @endauth
 
                     <a href="https://github.com/HarukiHashimoto/sys_lara">GitHub</a>
