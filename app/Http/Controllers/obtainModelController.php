ebcb67bbdc7914120b7aae5cc3c7b0de02b192f0
Haruki-MacMini <coral.reef610@gmail.com>
2018-04-20 21:55:44 +0900

    feat: mode情報を整形して表示するように

--- app/Http/Controllers/obtainModelController.php
+++ app/Http/Controllers/obtainModelController.php
@@ -3,21 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\UserModel;
-use Illuminate\Http\Request;
+use Request;
 
 // オブジェクトをJSON形式へ変換する（日本語をunicodeのままで整形して．）
 function json_safe_encode($data){
     return json_encode($data, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 }
 
+function search_q_a($user_model){
+    print_r($user_model);
+}
+
 class obtainModelController extends Controller
 {
     public function more_suggestion()
     {
-        // $m_title = Request::post('title');
-        $m_title = "ir";
-
-        // $m_title = "genpatsu";
+        $m_title = Request::input('title');
+        echo "検索対象: ".$m_title."<br />";
+        // $m_title = "ir";
         // ログイン中のユーザー以外の作成したモデルを取得
         $res = UserModel::select('user_id')
                 ->where('m_title', $m_title)
@@ -35,15 +38,23 @@ class obtainModelController extends Controller
                         ->first();
             }
 
+
+
             for ($i=0; $i < count($other); $i++) {
                 $user[$i] = $other[$i]->user_id;
                 $model[$i] = json_decode($other[$i]->model_json);
-                echo $user[$i].":<br />";
-                print_r($model[$i]->tagList);
+                print_r($model[$i]->nodes);
+                echo "ユーザID: ".$user[$i].":<br />";
+                echo "<pre>";
+                search_q_a($model[$i]);
+                echo "</pre>";
                 echo "<br />";
+            }
 
+            for ($i=0; $i < count($other); $i++) {
 
             }
+
             // ランダムでキー取得
             // $key = array_rand($other, 1);
             //
