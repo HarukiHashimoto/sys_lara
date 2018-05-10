0f0236f3f772edce7e50b13e895cb822b2c86df7
Haruki-MacMini <coral.reef610@gmail.com>
2018-05-10 19:12:36 +0900

    feat: add search_question function

--- app/Http/Controllers/obtainModelController.php
+++ app/Http/Controllers/obtainModelController.php
@@ -10,18 +10,37 @@ function json_safe_encode($data){
     return json_encode($data, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 }
 
-function search_q_a($user_model){
-    echo "<pre>";
-    // print_r($user_model->nodes);
-    foreach ($user_model->nodes->_data as $node) {
-        // print_r($node);
-        if ($node->group == 'instance') {
-            echo $node->label."<br />";
-            echo 'これは  「  <strong>'.$node->title.'</strong>  」  のインスタンスです<br /><br />';
+function search_q_a($user_id, $user_model){
+    for ($i=0; $i < count($user_id); $i++) {
+        echo "<pre>";
+        // print_r($user_model->nodes);
+        foreach ($user_model[$i]->nodes->_data as $node) {
+            // print_r($node);
+            if ($node->group == 'instance') {
+                echo $user_id[$i]."<br />";
+                echo $node->label."<br />";
+                echo 'これは  「  <strong>'.$node->title.'</strong>  」  のインスタンスです<br /><br />';
+                get_q_link($node, $user_model[$i]);
+            }
+        }
+
+
+        echo "</pre>";
+    }
+}
+
+function get_q_link($q_node, $user_model){
+    echo "<br />ここ";
+
+    // 問いを"to"に持つエッジを検索
+    foreach($user_model->edges->_data as $edge) {
+        if ($edge->to == $q_node->id) {
+            print_r($edge->from);
+            print_r($user_model->nodes->_data[$edge->to]);
         }
     }
-    print_r($user_model);
-    echo "</pre>";
+    echo "<br />";
+
 }
 
 class obtainModelController extends Controller
@@ -29,12 +48,14 @@ class obtainModelController extends Controller
     public function more_suggestion()
     {
         $m_title = Request::input('title');
+        $user_id = Request::input('user_id');
+
         echo "検索対象: ".$m_title."<br />";
-        // $m_title = "ir";
+
         // ログイン中のユーザー以外の作成したモデルを取得
         $res = UserModel::select('user_id')
                 ->where('m_title', $m_title)
-                // ->whereNotIn('user_id', [$user_id, 'given'])
+                ->whereNotIn('user_id', [$user_id, 'given'])
                 ->groupBy('user_id')
                 ->get();
 
@@ -52,12 +73,10 @@ class obtainModelController extends Controller
                 $user[$i] = $other[$i]->user_id;
                 $model[$i] = json_decode($other[$i]->model_json);
 
-                echo "ユーザID: ".$user[$i].":<br />";
-
-                search_q_a($model[$i]);
-
                 echo "<br />";
             }
+
+            search_q_a($user, $model);
 
             // for ($i=0; $i < count($other); $i++) {
             //
